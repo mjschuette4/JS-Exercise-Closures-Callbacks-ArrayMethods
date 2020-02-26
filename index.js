fa060a246e4daa07905c09a4537b46f025f3edc6
MatthewSchuette <mjschuette4@gmail.com>
2020-02-25 18:11:36 -0600

    strech finished

--- index.js
+++ index.js
@@ -156,8 +156,9 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  var deDuped = [... new Set(list)];
+  return callback(deDuped);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
